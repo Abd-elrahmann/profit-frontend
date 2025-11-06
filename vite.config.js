3010c6efa77ce8f8644b422e41bedbdfcd96ed38
Abd-elrahmann <cds.abdelrahmanmohamed74057@alexu.edu.eg>
2025-11-06 23:44:36 +0200

    config vire

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: 'src/',
+        replacement: `${path.resolve(__dirname, 'src')}/`,
+      },
+    ],
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
+  },
   server: {
     port: 3001,
     open: true,
@@ -10,8 +23,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    minify: 'esbuild',
     chunkSizeWarningLimit: 1000,
   },
   base: "./",
